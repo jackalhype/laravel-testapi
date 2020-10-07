ff26f381b932b71b1935cfa0d8f875b281bee0d2
kc <kcpi9000@yandex.ru>
2020-10-07 23:11:36 +0300

    no private previous private

--- docker/dev-init.sh
+++ docker/dev-init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+source ../.env
+
+if [ -z $HOST_DB_STORAGE_ROOT ]; then
+    echo "HOST_DB_STORAGE_ROOT not set in .env"
+    exit 1
+fi
+
 # host pgadmin volumes:
-sudo mkdir -p /private/var/lib/pgadmin && sudo chmod -R 0777 /private/var/lib/pgadmin
+sudo mkdir -p "${HOST_DB_STORAGE_ROOT}"/var/lib/pgadmin && sudo chmod -R 0777 "${HOST_DB_STORAGE_ROOT}"/var/lib/pgadmin
 
 # host postgres volumes:
-sudo mkdir -p /private/var/lib/postgresql/data && chmod -R 0777 /private/var/lib/postgresql/data
+sudo mkdir -p "${HOST_DB_STORAGE_ROOT}"/var/lib/postgresql/data && chmod -R 0777 "${HOST_DB_STORAGE_ROOT}"/var/lib/postgresql/data
