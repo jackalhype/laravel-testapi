f55bf10d071ba09b3cbd56037e63d1dd70753b15
kc <kcpi9000@yandex.ru>
2020-10-13 19:58:48 +0300

    race condition pessimistic

--- app/Http/Services/DocumentUpdateService.php
+++ app/Http/Services/DocumentUpdateService.php
@@ -5,18 +5,23 @@ namespace App\Http\Services;
 use App\Enums\DocumentStatus;
 use App\Http\Resources\DocumentResource;
 use App\Models\Document;
+use Illuminate\Support\Facades\DB;
 
 class DocumentUpdateService
 {
     public function update(Document $document, array $data) : DocumentResource
     {
-        $payload = $data['document']['payload'] ?? [];
-        $doc_payload = $document->payload;
-        foreach($payload as $k => $v) {
-            $doc_payload[$k] = $v;
-        }
-        $document->payload = $doc_payload;
-        $document->saveOrFail();
-        return new DocumentResource($document);
+        return DB::transaction(function() use ($document, $data) {
+            $doc = Document::where('id', '=', $document->id)
+                ->lockForUpdate()->first();
+            $payload = $data['document']['payload'] ?? [];
+            $doc_payload = $doc->payload;
+            foreach($payload as $k => $v) {
+                $doc_payload[$k] = $v;
+            }
+            $doc->payload = $doc_payload;
+            $doc->saveOrFail();
+            return new DocumentResource($doc);
+        }, 2);
     }
 }
--- app/Models/Document.php
+++ app/Models/Document.php
@@ -14,6 +14,7 @@ class Document extends UuidModel
     use HasFactory;
 
     protected $casts = [
+        'id' => 'string',
         'payload' => 'array'
     ];
 }
